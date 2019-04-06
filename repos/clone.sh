9cebc96396550c0ea20ee7a9c00b06de65537bd4
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-04-06 16:58:06 +0300

    Fix clone script (again)

--- repos/clone.sh
+++ repos/clone.sh
@@ -5,9 +5,9 @@ for repo in hoover/snoop2 hoover/search liquidinvestigations/core; do
     echo $repo
     mkdir -p $repo
     if [ "$1" == 'ssh' ]; then
-            git clone "https://github.com/$repo.git" $repo
-    elif [ "$1" == 'https' ]; then
             git clone "git@github.com:$repo.git" $repo
+    elif [ "$1" == 'https' ]; then
+            git clone "https://github.com/$repo.git" $repo
     else
             set +x
             echo "Usage: $0 https - clone repos with https"
