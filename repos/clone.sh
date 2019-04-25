d104cb33c9e2a1608b448273b8b2e9e5b42de932
Mugur Rus <mugur.rus@gmail.com>
2019-04-25 13:47:02 +0300

    restore the repos directory

---
+++ repos/clone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+for repo in hoover/snoop2 hoover/search liquidinvestigations/core; do
+    echo $repo
+    mkdir -p $repo
+    if [ "$1" == 'ssh' ]; then
+            git clone "git@github.com:$repo.git" $repo
+    elif [ "$1" == 'https' ]; then
+            git clone "https://github.com/$repo.git" $repo
+    else
+            set +x
+            echo "Usage: $0 https - clone repos with https"
+            echo "       $0 ssh   - clone repos with ssh"
+            exit 1
+    fi
+done
