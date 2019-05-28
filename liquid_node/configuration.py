5a1aabe42793d03a31c0f310acefc31bb4036f62
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2019-05-28 05:34:23 +0300

    wait 10 minutes

--- liquid_node/configuration.py
+++ liquid_node/configuration.py
@@ -78,7 +78,6 @@ class Configuration:
 
         self.check_interval = self.ini.get('deploy', 'check_interval', fallback='3s')
         self.check_timeout = self.ini.get('deploy', 'check_timeout', fallback='2s')
-        self.check_timeout = self.ini.get('deploy', 'check_timeout', fallback='2s')
         self.wait_max = self.ini.getfloat('deploy', 'wait_max_sec', fallback=300)
         self.wait_interval = self.ini.getfloat('deploy', 'wait_interval', fallback=1)
         self.wait_green_count = self.ini.getint('deploy', 'wait_green_count', fallback=10)
