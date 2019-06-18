329269ab1c4109028c768846979b37370e298e73
Alex Morega <alex@grep.ro>
2019-06-18 18:48:49 +0300

    Land the local jobs in `./local_jobs/`

---
+++ examples/litterbox.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+class Cat
+    def __init__(self, name, job_config, config):
+        self.name = name
+        self.template = Path(__file__).parent.resolve() / 'cat.hlc'
