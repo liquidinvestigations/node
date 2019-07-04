87d84d30372b11a1084bd285c60a06c0f5f49b04
Radu Capalb <36372694+raduklb@users.noreply.github.com>
2019-07-04 17:50:23 +0300

    Remove symlinks creation from vagrant/provision-liquid.sh (#132)
    * Remove symlinks creation from vagrant/provision-liquid.sh
    
    * Extract mkdir from 'if's and delete 'if's

--- vagrant/provision-liquid.sh
+++ vagrant/provision-liquid.sh
@@ -7,19 +7,9 @@ echo "Installing liquid" > /dev/null
 cd /opt/node
 sudo chown -R vagrant: .
 
-if ! [ -e volumes ]; then
-  mkdir -p /opt/var/node/volumes
-  rm -f volumes
-  ln -s /opt/var/node/volumes
-fi
-
-if ! [ -e collections ]; then
-  mkdir -p /opt/var/node/collections
-  rm -f collections
-  ln -s /opt/var/node/collections
-fi
-
-if ! [ -e collections/testdata ]; then
+mkdir volumes
+mkdir collections
+if ! [ -d collections/testdata ]; then
   git clone https://github.com/hoover/testdata collections/testdata
 fi
 
