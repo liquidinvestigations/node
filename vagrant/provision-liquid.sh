546323ed671689d474902d58bab7bc23cb18f1a8
Radu Capalb <radu_capalb98@yahoo.com>
2019-07-24 12:07:19 +0300

    Add sudo -H when installing Pipenv

--- vagrant/provision-liquid.sh
+++ vagrant/provision-liquid.sh
@@ -16,7 +16,7 @@ fi
 cp examples/liquid.ini .
 
 sudo apt install python3-venv python3-pip
-pip3 install pipenv
+sudo -H pip3 install pipenv
 pipenv install -r requirements.txt
 pipenv run ./liquid.py resources
 pipenv run ./liquid.py deploy
