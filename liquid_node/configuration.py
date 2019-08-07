f3c0e819bcc53a07bb665463c948f925bc581b8a
MattesR <42345537+MattesR@users.noreply.github.com>
2019-08-07 16:08:50 +0200

    Tika memory from ini (#165)
    * set tika memory in liquid.ini (default = 800)
    
    * change memory in hoover-deps
    
    * add description to Readme
    
    * sorry, flake8
    
    * remove substitution, change name to tika_memory_limit

--- liquid_node/configuration.py
+++ liquid_node/configuration.py
@@ -101,6 +101,8 @@ class Configuration:
         self.elasticsearch_memory_limit = self.ini.getint('liquid', 'elasticsearch_memory_limit',
                                                           fallback=2048)
 
+        self.tika_memory_limit = self.ini.get('liquid', 'tika_memory_limit', fallback=800)
+
         self.check_interval = self.ini.get('deploy', 'check_interval', fallback='3s')
         self.check_timeout = self.ini.get('deploy', 'check_timeout', fallback='2s')
         self.wait_max = self.ini.getfloat('deploy', 'wait_max_sec', fallback=300)
