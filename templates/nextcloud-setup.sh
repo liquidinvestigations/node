be1b0c5123f91a45bfc8d6d8ece591f200eef2de
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-01-08 02:30:53 +0200

    php occ still can't reset its own dbhost from cli
    When the config.php file has an old link.

--- templates/nextcloud-setup.sh
+++ templates/nextcloud-setup.sh
@@ -2,13 +2,17 @@
 
 set -ex
 
+sleep 7
+
 if [ -z "$MYSQL_HOST" ]; then
     echo "Missing MYSQL_HOST - please wait for the DB to spin up before running setup"
-    sleep 6
     exit 1
 fi
 
-INSTALLED=$(php /var/www/html/occ status --output=json | jq .installed)
+set +e
+INSTALLED=$(php /var/www/html/occ status --output=json | jq '.installed' || echo "error")
+set -e
+
 if [ "$INSTALLED" == "false" ]; then
     echo "Installing nextcloud"
 
@@ -32,32 +36,11 @@ if [ "$INSTALLED" == "false" ]; then
 
     echo "Installation successful -- now restarting (aka failing) the migrate job"
     exit 66
-elif [ "$INSTALLED" == "null" ]; then
-    echo "Apache probably not accepting host header"
-    sleep 6
-    exit 1
 fi
-(
-set +x
-export OC_PASS="$UPLOADS_USER_PASSWORD"
-php occ user:resetpassword --password-from-env uploads
-export OC_PASS="$NEXTCLOUD_ADMIN_PASSWORD"
-php occ user:resetpassword --password-from-env $NEXTCLOUD_ADMIN_USER
-)
-echo "uploads and admin password set
-"
-echo "Unpacking theme"
-
-rm -rf /var/www/html/themes/liquid || true
-cp -r /liquid/theme /var/www/html/themes/liquid
-chown -R www-data:www-data /var/www/html/themes/liquid
-chmod g+s /var/www/html/themes/liquid
 
 echo "Configuring..."
-
 php occ config:system:set trusted_domains 0 --value '*'
 php occ config:system:set dbhost --value $MYSQL_HOST
-php occ config:system:set theme --value liquid
 php occ config:system:set overwrite.cli.url --value $HTTP_PROTO://$NEXTCLOUD_HOST
 php occ config:system:set allow_user_to_change_display_name --value false --type boolean
 php occ config:system:set overwritehost --value $NEXTCLOUD_HOST
@@ -65,12 +48,19 @@ php occ config:system:set overwriteprotocol --value $HTTP_PROTO
 php occ config:system:set htaccess.RewriteBase --value '/'
 php occ config:system:set skeletondirectory --value ''
 php occ config:system:set updatechecker --value false --type boolean
+php occ config:system:set has_internet_connection --value false --type boolean
+php occ config:system:set appstoreenabled --value false --type boolean
+
+echo "Unpacking theme"
+rm -rf /var/www/html/themes/liquid || true
+cp -r /liquid/theme /var/www/html/themes/liquid
+chown -R www-data:www-data /var/www/html/themes/liquid
+chmod g+s /var/www/html/themes/liquid
+
+php occ config:system:set theme --value liquid
 
 #install contacts before shutting down the app store
 php occ app:install contacts || true
-
-php occ config:system:set has_internet_connection --value false --type boolean
-php occ config:system:set appstoreenabled --value false --type boolean
 
 php occ app:disable theming
 php occ app:disable accessibility
@@ -93,6 +83,16 @@ php occ app:disable systemtags
 php occ app:disable updatenotification
 
 echo "Configuration done"
+
+(
+set +x
+export OC_PASS="$UPLOADS_USER_PASSWORD"
+php occ user:resetpassword --password-from-env uploads
+export OC_PASS="$NEXTCLOUD_ADMIN_PASSWORD"
+php occ user:resetpassword --password-from-env $NEXTCLOUD_ADMIN_USER
+)
+echo "uploads and admin password set
+"
 
 # scan the filesystem in case there are files initially (redeploy e.g.)
 php occ files:scan --all
