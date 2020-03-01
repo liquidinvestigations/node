d9c7dd9df51d4ee76711e4023afd44601c249b38
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-03-01 18:58:11 +0200

    some paths have spaces

--- update-0.8-0.9.sh
+++ update-0.8-0.9.sh
@@ -12,18 +12,20 @@ if [[ -z "$LIQUID_VOLUMES" || -z "$LIQUID_COLLECTIONS" || -z "$LIQUID_COLLECTION
         exit 1
 fi
 
-for old in $(find -L "$LIQUID_COLLECTIONS" -mindepth 1 -maxdepth 1 -type d,l); do
+while IFS= read -r -d '' old; do
+       echo "$old"
         name="$(basename "$old")"
         new="$LIQUID_COLLECTIONS_NEW/$name/data"
 
         mkdir -p "$new"
+       (
+               while IFS= read -r -d '' item; do
+                       mv -v "$item" -t "$new"
+               done < <(find -L "$old" -mindepth 1 -maxdepth 1 -print0)
+       )
 
-        set +x
-        for item in $(find -L $old -maxdepth 1 -mindepth 1); do
-                mv -v $item -t $new
-        done
-        set -x
-done
+done < <(find -L "$LIQUID_COLLECTIONS" -mindepth 1 -maxdepth 1 -type d,l -print0)
+
 
 for collection in $(find -L "$LIQUID_VOLUMES/collections" -mindepth 1 -maxdepth 1 -type d,l); do
         name="$(basename "$collection")"
