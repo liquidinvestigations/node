7549548ad4806c199cb50b59d99157f7879cbc00
Morten Stehr <morten.stehr@spiegel.de>
2020-04-15 09:30:55 +0200

    db as own job

--- liquid_node/jobs/nextcloud.py
+++ liquid_node/jobs/nextcloud.py
@@ -6,6 +6,11 @@ class Nextcloud(jobs.Job):
     template = jobs.TEMPLATES / f'{name}.nomad'
     app = 'nextcloud'
 
+class Database(jobs.Job):
+    name = 'nextcloud-database'
+    template = jobs.TEMPLATES / f'{name}.nomad'
+    app = 'nextcloud'
+
 
 class Migrate(jobs.Job):
     name = 'nextcloud-migrate'
