075e42e8e016e034a4aae8e4682e243febe609a0
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-04-28 03:52:20 +0300

    fix flake8 issues

--- liquid_node/jobs/nextcloud.py
+++ liquid_node/jobs/nextcloud.py
@@ -6,15 +6,18 @@ class Nextcloud(jobs.Job):
     template = jobs.TEMPLATES / f'{name}.nomad'
     app = 'nextcloud'
 
+
 class Migrate(jobs.Job):
     name = 'nextcloud-migrate'
     template = jobs.TEMPLATES / f'{name}.nomad'
     app = 'nextcloud'
 
+
 class Periodic(jobs.Job):
     name = 'nextcloud-periodic'
     template = jobs.TEMPLATES / f'{name}.nomad'
     app = 'nextcloud'
+
 
 class Database(jobs.Job):
     name = 'nextcloud-database'
