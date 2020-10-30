734084350809944b157a89a5021362ebab95c316
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2020-10-30 15:21:09 +0200

    route the UI and backend through a new nginx

--- liquid_node/configuration.py
+++ liquid_node/configuration.py
@@ -158,6 +158,7 @@ class Configuration:
         self.hoover_web_count = self.ini.getint('liquid',
                                                 'hoover_web_count', fallback=1)
 
+        self.hoover_ui_override_server = self.ini.get('liquid', 'hoover_ui_override_server', fallback='')
         self.snoop_workers_enabled = self.ini.getboolean('snoop', 'enable_workers', fallback=True)
         self.snoop_min_workers_per_node = self.ini.getint('snoop', 'min_workers_per_node', fallback=2)
         self.snoop_max_workers_per_node = self.ini.getint('snoop', 'max_workers_per_node', fallback=4)
