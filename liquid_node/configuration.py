74610881ac563572b19132ffce98577a34a5e519
MattesR <mattes.ruckdeschel@tuhh.de>
2021-04-30 11:03:59 +0200

    flake

--- liquid_node/configuration.py
+++ liquid_node/configuration.py
@@ -160,7 +160,6 @@ class Configuration:
         self.tika_count = self.ini.getint('liquid', 'tika_count', fallback=1)
         self.tika_memory_limit = self.ini.getint('liquid', 'tika_memory_limit', fallback=800)
 
-
         self.hypothesis_memory_limit = \
             self.ini.getint('liquid',
                             'hypothesis_memory_limit',
