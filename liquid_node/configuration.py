42d7ffb5c24037b9f06956e99844cc8b882d3ac4
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2022-01-28 20:19:37 +0200

    adjust memory requirements for snoop workers

--- liquid_node/configuration.py
+++ liquid_node/configuration.py
@@ -194,9 +194,9 @@ class Configuration:
         self.snoop_rabbitmq_memory_limit = self.ini.getint('snoop', 'rabbitmq_memory_limit', fallback=700)
         self.snoop_postgres_memory_limit = self.ini.getint('snoop', 'postgres_memory_limit', fallback=1400)
         self.snoop_postgres_max_connections = self.ini.getint('snoop', 'postgres_max_connections', fallback=250)  # noqa: E501
-        self.snoop_worker_memory_limit = 500 * (2 + self.snoop_min_workers_per_node)
-        self.snoop_worker_hard_memory_limit = 5000 * (2 + self.snoop_max_workers_per_node)
-        self.snoop_worker_cpu_limit = 1500 * self.snoop_min_workers_per_node
+        self.snoop_worker_memory_limit = 400 * (1 + self.snoop_min_workers_per_node)
+        self.snoop_worker_hard_memory_limit = 2000 * (2 + self.snoop_max_workers_per_node)
+        self.snoop_worker_cpu_limit = 400 * self.snoop_min_workers_per_node
         self.snoop_max_result_window = self.ini.getint('snoop', 'max_result_window', fallback=10000)
         self.snoop_refresh_interval = self.ini.get('snoop', 'refresh_interval', fallback="6s")
 
