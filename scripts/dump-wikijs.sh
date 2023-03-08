84178eee12ae07543263e82c772795441c2c9457
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-03-08 20:43:37 +0200

    cleanup sql templates

--- scripts/dump-wikijs.sh
+++ scripts/dump-wikijs.sh
@@ -4,4 +4,4 @@ cd "$(dirname ${BASH_SOURCE[0]})/.."
 
 ./liquid shell wikijs:wikijs \
     bash -c 'PGPASSWORD=$DB_PASS pg_dumpall --no-password --dbname "$WIKIJS_DB" --database "$DB_NAME" --no-owner --inserts --column-inserts --no-acl' \
-    > ./templates/wikijs-db-init.sql
+    > ./templates/wikijs-db-init-orig-dump.sql
