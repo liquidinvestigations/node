a409d4e8c5d347db240eedb06fb6b9560f83b967
Kjell Knudsen <kjell.knudsen@yahoo.de>
2023-03-24 10:27:30 +0100

    simplify script and use a more robust approach to get zip file into the wikjs container

--- scripts/export-dokuwiki-into-wikijs.sh
+++ scripts/export-dokuwiki-into-wikijs.sh
@@ -1,16 +1,18 @@
 #!/bin/bash -ex
 
-# code snippet comes from https://feedback.js.wiki/wiki/p/import-from-another-wiki and https://stackoverflow.com/a/4774063
+# code snippet comes from https://feedback.js.wiki/wiki/p/import-from-another-wiki
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-echo $SCRIPTPATH
+cd "$(dirname ${BASH_SOURCE[0]})/.."
+
 # requires bash >= 4.0 to loop through subdirectories
 shopt -s globstar
 
 mkdir -p /tmp/dokuwiki-export
-cd "${SCRIPTPATH}/../volumes/dokuwiki/data/dokuwiki/data/pages"
-echo "Copying dokuwiki content to temporary location for conversion..."
-cp -a ./. /tmp/dokuwiki-export
+(
+    cd "./volumes/dokuwiki/data/dokuwiki/data/pages"
+    echo "Copying dokuwiki content to temporary location for conversion..."
+    cp -a ./. /tmp/dokuwiki-export
+)
 
 echo "Converting all pages..."
 for d in /tmp/dokuwiki-export/**/*.txt; do
@@ -25,22 +27,26 @@ echo "Conversion done!"
 shopt -u globstar
 
 echo "Creating zip archive of all pages..."
-cd /tmp/dokuwiki-export
-zip -r "${SCRIPTPATH}/../dokuwiki-export.zip"  .
+(
+    cd /tmp/dokuwiki-export
+    zip -r "../dokuwiki-export.zip"  .
+)
 echo "Zip created!"
+
 
 echo "Removing temporary files..."
 rm -r /tmp/dokuwiki-export
 
 echo "Export finished!"
 
-CONTAINER_ID=$(docker ps | grep "liquidinvestigations/wiki.js" | awk '{print $1}')
-
-cd "${SCRIPTPATH}/../"
-echo "Uploading the data to wikijs...!"
 ./liquid shell wikijs:wikijs bash -c 'mkdir -p /tmp/wiki'
-docker cp ${SCRIPTPATH}/../dokuwiki-export.zip ${CONTAINER_ID}:/tmp/wiki
+
+echo "Uploading the data to wikijs...!"
+cat /tmp/dokuwiki-export.zip | docker exec -i cluster ./cluster.py nomad-exec wikijs:wikijs -- bash -c "cat > /tmp/wiki/dokuwiki-export.zip"
 ./liquid shell wikijs:wikijs bash -c 'unzip /tmp/wiki/dokuwiki-export.zip -d /tmp/wiki && rm /tmp/wiki/dokuwiki-export.zip'
 
+rm -r /tmp/dokuwiki-export.zip
+
 echo "Done!"
+
 echo "Go to your wiki.js admin interface and under 'Storage' enable 'Local File System' with the path /tmp/wiki. Press import everything at the bottom to import all the pages. After that you can disable the 'Local File System again'."
