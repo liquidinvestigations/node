0c845683102b979c71fe4c06dd68ad9851538ba6
Gabriel Vîjială <gabriel.vijiala@gmail.com>
2023-09-29 18:05:20 +0300

    fix liquid migrate users script for wikijs

--- scripts/migrate_wikijs_users.sh
+++ scripts/migrate_wikijs_users.sh
@@ -1,15 +1,16 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
 cd "$(dirname ${BASH_SOURCE[0]})/.."
 
-USERS=$(./liquid shell liquid:core \
-         bash -c 'sqlite3 ./var/db.sqlite3 "select username from auth_user;"')
+export USERS="$(./liquid dockerexec liquid:core bash -c 'sqlite3 ./var/db.sqlite3 "select username from auth_user;"')"
 
-echo $LIQUID_DOMAIN
+echo "USERS: $USERS"
 
 for u in $USERS; do
     # sqlite outputs the names like: username\r so we remove the trailing carriage return.
     LIQUID_USER=$(echo $u | sed -e 's/\r//')
+    echo $LIQUID_USER
+
     # $LIQUID_DOMAIN is an environment variable inside the wikijs container so we escape it here
     ./liquid shell wikijs:wikijs \
              bash -c "node /wiki/create-user.js $LIQUID_USER@\$LIQUID_DOMAIN $LIQUID_USER || true"
